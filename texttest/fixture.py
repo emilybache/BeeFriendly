12545295268af6f38aa37009f5730e02e4d8a029
Emily Bache <emily@bacheconsulting.com>
2020-06-09 15:03:53 +0000

    first draft of smoke test

--- texttest/fixture.py
+++ texttest/fixture.py
@@ -1,11 +1,28 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
-driver.get('http://www.google.com/')
-time.sleep(5) # Let the user actually see something!
-search_box = driver.find_element_by_name('q')
-search_box.send_keys('ChromeDriver')
-search_box.submit()
-time.sleep(5) # Let the user actually see something!
-driver.quit()
+
+def run_selenium():
+    driver = webdriver.Chrome()
+    driver.get("http://localhost:8080/")
+
+    print_html_page(driver, "start_page")
+
+    dropdown = driver.find_element(By.CSS_SELECTOR, "select")
+    dropdown.find_element(By.XPATH, "//option[. = 'Balcony']").click()
+    driver.find_element(By.CSS_SELECTOR, ".form-group:nth-child(2) > .btn").click()
+
+    print_html_page(driver, "end_page")
+
+    driver.quit()
+
+
+def print_html_page(driver, page_name):
+    html = driver.page_source
+    with open(f"{page_name}.html", "w", encoding="utf-8") as f:
+        f.write(html)
+
+
+if __name__ == "__main__":
+    run_selenium()
