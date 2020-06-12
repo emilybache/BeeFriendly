f82054113ce4e453c085d7eb888d846a8fe93f43
Emily Bache <emily@bacheconsulting.com>
2020-06-12 15:44:30 +0000

    make the users database fit the bee theme

--- users/database.sql
+++ users/database.sql
@@ -11,9 +11,11 @@ CREATE TABLE IF NOT EXISTS users.people (
 
 DELETE FROM users.people;
 
-INSERT INTO users.people VALUES ('Gru', 'Felonius', 'Where are the minions?');
-INSERT INTO users.people VALUES ('Nefario', 'Dr.', 'Why ... why are you so old?');
-INSERT INTO users.people VALUES ('Agnes', '', 'Your unicorn is so fluffy!');
-INSERT INTO users.people VALUES ('Edith', '', "Don't touch anything!");
-INSERT INTO users.people VALUES ('Vector', '', 'Committing crimes with both direction and magnitude!');
-INSERT INTO users.people VALUES ('Dave', 'Minion', 'Ngaaahaaa! Patalaki patalaku Big Boss!!');
+INSERT INTO users.people VALUES ('Charles Darwin', 'Dr.', 'On the origin of species');
+INSERT INTO users.people VALUES ('Carl Linnaeus', 'Prof.', 'Species Plantarum');
+INSERT INTO users.people VALUES ('Sophie Lutterlough', 'Mrs.', 'Pygmephorus lutterloughae');
+INSERT INTO users.people VALUES ('Karl von Frisch', 'Dr.', 'The Dancing Bees: An Account of the Life and Senses of the Honey Bee');
+INSERT INTO users.people VALUES ('Dave Goulson', 'Prof.', 'Bumblebees: Their Behaviour and Ecology');
+INSERT INTO users.people VALUES ('Charles Duncan Michener', 'Dr.', 'The Social Behavior of the Bees');
+INSERT INTO users.people VALUES ('Emily Dickinson', 'Ms.', "The Bumble-Bee's Religion");
+INSERT INTO users.people VALUES ('Nikolai Rimsky-Korsakov', 'Mr.', 'Flight of the Bumblebee');
