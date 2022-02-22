c98f330112c96fadd6c6b97b4976c66bc291ee79
Emily Bache <emily@bacheconsulting.com>
2022-02-22 10:40:54 +0000

    begin bee friendly dev

--- bee-friendly-ui-2/src/App.vue
+++ bee-friendly-ui-2/src/App.vue
@@ -6,11 +6,8 @@ import TheWelcome from "./components/TheWelcome.vue";
 <template>
   <header>
     <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
+      alt="Bee Friendly banner"
+      src="./assets/azalea_banner.jpg"
     />
 
     <div class="wrapper">
